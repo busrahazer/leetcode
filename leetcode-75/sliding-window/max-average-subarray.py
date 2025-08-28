29bd27fa89227df7e1ecdb45f9cf866a303557ef
busra <minebusrahazer@gmail.com>
2025-08-28 21:48:16 +0300

    Algorithm-14

---
+++ leetcode-75/sliding-window/max-average-subarray.py
@@ -0,0 +1,28 @@
+"""You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+ """
+
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+
+        for i in range(k, len(nums)):
+            window_sum = window_sum - nums[i - k] + nums[i]
+            max_sum = max(max_sum, window_sum)
+
+        return max_sum / float(k)
